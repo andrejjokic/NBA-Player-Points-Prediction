df67110cc3e00f9c359cbdca25ac073c907a08da
Andrej Jokic <72042091+andrejjokic@users.noreply.github.com>
2021-11-08 11:40:55 +0100

    Added gui and pushed app to cloud

--- Implementation/main.py
+++ Implementation/main.py
@@ -22,28 +22,62 @@ def main(player, team, opponent, season_type, home_game, b2b=None):
 
     # Output predicted points
     print("Predicted points: ", pts)
+    
+    return pts
 
 
+def main_wrapper():
+    text = ''
+    try:
+        home_game = 1 if st.session_state.home_game == 'Home' else 0
+        b2b = 1 if st.session_state.b2b else 0
+        pts = main(st.session_state.player, st.session_state.team, st.session_state.opponent, st.session_state.season_type, home_game, b2b)
+        pts = round(pts)
+        text = f'Predicted points: {pts}'
+    except:
+        text = 'Invalid input'
+
+    st.markdown(f"<h1 style='color:lightgreen'>{text}</h1>", unsafe_allow_html=True)
+
+    
 if __name__ == "__main__":
-    # Ignore WARNINGs 
-    warnings.filterwarnings('ignore')
-    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+    # Set app title
+    st.title("Predict player's points in a game!")
 
-    # Create command line parser
-    parser = argparse.ArgumentParser()
+    with st.form('Input Form', clear_on_submit=False):
+        # Input data
+        st.text_input('Player', key='player', help="Player's name")
+        st.text_input('Team', key='team', help="Player's team name")
+        st.text_input('Opponent', key='opponent', help="Opponent's team name")
+        st.selectbox('Season Type', ['Regular Season', 'Playoffs'], key='season_type', help="Regular Season or Playoff game")
+        st.selectbox('Playing', ['Home', 'Away'], key='home_game', help='Home or Away game')
+        st.checkbox('Back to Back Game', key='b2b')
 
-    # Add command line arguments
-    parser.add_argument('--player', '-p', help='<Required> Player Name', required=True)
-    parser.add_argument('--team', '-t', help='<Required> Team Name', required=True)
-    parser.add_argument('--opponent', '-o', help='<Required> Opponent Team Name', required=True)
-    parser.add_argument('--season_type', '-s', help='<Required> Regular Season or Playoffs', required=True)
-    parser.add_argument('--home_game', '-hg', help='<Required> 1 = Home, 0 = Away', required=True)
-    parser.add_argument('--b2b', '-b', help='<Required> 1 = True, 0 = False')
+        # Submit
+        st.form_submit_button('Predict', help='Predict player points!', on_click=main_wrapper)
 
-    # Parse arguments
-    args = parser.parse_args()
+        
+        
+# if __name__ == "__main__":
+#     # Ignore WARNINGs 
+#     warnings.filterwarnings('ignore')
+#     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 
-    # Run main
-    main(args.player, args.team, args.opponent, args.season_type, args.home_game, args.b2b)
+#     # Create command line parser
+#     parser = argparse.ArgumentParser()
+
+#     # Add command line arguments
+#     parser.add_argument('--player', '-p', help='<Required> Player Name', required=True)
+#     parser.add_argument('--team', '-t', help='<Required> Team Name', required=True)
+#     parser.add_argument('--opponent', '-o', help='<Required> Opponent Team Name', required=True)
+#     parser.add_argument('--season_type', '-s', help='<Required> Regular Season or Playoffs', required=True)
+#     parser.add_argument('--home_game', '-hg', help='<Required> 1 = Home, 0 = Away', required=True)
+#     parser.add_argument('--b2b', '-b', help='<Required> 1 = True, 0 = False')
+
+#     # Parse arguments
+#     args = parser.parse_args()
+
+#     # Run main
+#     main(args.player, args.team, args.opponent, args.season_type, args.home_game, args.b2b)
 
 
